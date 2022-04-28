28c7ef0e2a9d5fd6a09736a978d52191747a027b
Jun Kakishima <adventureocean3699@gmail.com>
2022-04-28 21:38:22 +0900

    add static css

--- jjj_blog/jjj_blog/settings.py
+++ jjj_blog/jjj_blog/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'my_blog.urls'
+ROOT_URLCONF = 'jjj_blog.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'my_blog.wsgi.application'
+WSGI_APPLICATION = 'jjj_blog.wsgi.application'
 
 
 # Database
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL =  'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- jjj_blog/jjj_blog/urls.py
+++ jjj_blog/jjj_blog/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
-from blog import views
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    path('', include('blog.urls')),
 ]
